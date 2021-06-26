3912621d7156e9d652e856d1b9d5a270a2791956
Carlos Petit <ca.alberto.p@gmail.com>
2021-06-26 17:24:40 -0400

    Update okemon class

--- main.py
+++ main.py
@@ -7,20 +7,32 @@ if __name__ == "__main__":
     cont = True
     selectedPokemon: Pokemon = None
     while cont:
-        name = input("Ingrese el nombre del primer Pokémon: ")
+        name = input(f"Ingrese el nombre del primer Pokémon: ")
         result = getCsvPokemonData(name)
         if(result != None):
             selectedPokemon, efectividad = initPokemon(result)
             cont = False
         print(
-            f"El Pokemon '{name}' no esta en nuestra data, intenta con otro.")
+            f"El Pokemon '{name}' no esta en nuestra Pokedex, intenta con otro. ")
     print(selectedPokemon)
     print("\n")
     selectedPokemon.setEfectividad(efectividad)
     selectedPokemon.imprimirEstadisticas()
     selectedPokemon.mostrarMovimientos()
-    mov = int(input("Seleccione un ataque a ejecutar: "))
-    movSelected = selectedPokemon.getMovimientoPorIndice(mov)
+    isValidAttak = True
+    while isValidAttak:
+        try:
+            mov = int(input("Seleccione un ataque a ejecutar: "))
+            movSelected = selectedPokemon.getMovimientoPorIndice(mov)
+            movPower = selectedPokemon.getStatMov(movSelected)
+            if(movPower > 0):
+                isValidAttak = False
+            else:
+                print("El ataque seleccionado no es valido, por favor selecciona otro ")
+        except ValueError:
+            print("El ataque seleccionado no es valido, por favor selecciona otro, recuerda que debes ingresar un numero  ")
+            pass
+
     print("El ataque seleccionado es: ", movSelected)
     print("Poder de ataque es: ", selectedPokemon.getStatMov(movSelected))
     selectedPokemon.imprimirEstadisticasAvanzadas()
